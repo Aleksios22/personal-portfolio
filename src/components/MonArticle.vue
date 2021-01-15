6cac5b3d8dd02440574935a6deceee546b83866f
Alexis <e1577094@cmaisonneuve.qc.ca>
2021-01-14 19:22:40 -0500

    petits travaux2

--- src/components/MonArticle.vue
+++ src/components/MonArticle.vue
@@ -1,11 +1,9 @@
 <template>
    <article>
-      <p v-for="tours in toursBoucle" v-bind:key="tours">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores,
-        distinctio doloribus iusto provident voluptas consequuntur assumenda
-        repellendus optio tenetur vquae sit porro praesentium molestiae? Labore,
-        ad cumque! Corrupti, sapiente ullam.
+      <p v-for="tours in toursBoucle" :key="tours">
+       bonsoir
       </p>
+
     </article>
 </template>
 
@@ -13,19 +11,24 @@
 export default {
     data() {
         return {
-            toursBoucle : 3
+            toursBoucle : 3,
+            commentaires : null
         }
     },
     beforeCreate() {
-        fetch('https://jsonplaceholder.typicode.com/posts')
+       
+       fetch('https://jsonplaceholder.typicode.com/posts')
         .then(reponse => reponse.json())
-        .then(resultat => this.posts = resultat)
+        .then(resultat => console.log(resultat));
+
     }
 }
 </script>
 
 <style>
+
     article {
-    border: 1px solid black;
+        border: 1px solid black;
     }
+
 </style>

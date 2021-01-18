e356c07a1aaae12332e2a2bd2017f2f1f64b71f8
Alexis <e1577094@cmaisonneuve.qc.ca>
2021-01-18 18:42:27 -0500

    redéploiement

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <main>
-    <h1>En construction ...</h1>
+    <h1>En Construction ...</h1>
   </main>
 </template>
+
+
 
 <script>
 export default {
@@ -11,13 +13,19 @@ export default {
 };
 </script>
 
+
+
 <style lang="scss">
-      @font-face {
-    font-family: 'Nimbus Mono bold';
-    src: local('Nimbus Mono bold'), url('./assets/Fonts/nimbus-mono/nimbusmono-bold.otf')  format('truetype'),
-  }
+  //     @font-face {
+  //   font-family: 'Nimbus Mono bold';
+  //   src: local('Nimbus Mono bold'), url('./assets/Fonts/nimbus-mono/nimbusmono-bold.otf')  format('truetype'),
+  // }
 #app{
-  font-family: 'Nimbus Mono bold';
+  width: 100vw;
+  height: 100vh;
+
+  background: #FEF7CB;
+  // font-family: 'Nimbus Mono bold';
   display: flex;
   justify-content: center;
   align-items: center;

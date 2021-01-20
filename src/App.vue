7506d7552fdf54b357f60cb326cbdcfaabe69bcf
Alexis <e1577094@cmaisonneuve.qc.ca>
2021-01-20 01:16:30 -0500

    Petits ajustements lien behance

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <main>
     <h1>En construction ...</h1>
+    <h2>Portfolio temporaire sur mon <a href="https://www.behance.net/alexisrochon1/projects">Behance</a></h2>
   </main>
 </template>
 
@@ -16,16 +17,60 @@ export default {
 
 
 <style lang="scss">
-
-#app{
+// --------------MOBILE FIRST---------------
+@font-face {
+  font-family: nimbusReg;
+  src: url("./assets/Fonts/nimbus-mono/nimbusmono-regular.otf");
+}
+@font-face {
+  font-family: nimbusBold;
+  src: url("./assets/Fonts/nimbus-mono/nimbusmono-bold.otf");
+}
+body {
+    background:#FEF7CB;
+    overflow: hidden;
+  #app{
   width: 100vw;
   height: 100vh;
-
-  background: #fff8c7;
+  font-family: "nimbusReg";
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 80%;
   margin: 0 auto;
+  main {
+    text-align: center;
+    h1 {
+      font-family: nimbusBold;
+      font-size: 1.8rem;
+    }
+    h2 {
+      font-size: 0.9rem;
+      a {
+        color: #85B36B;
+        font-size: 1.2rem;
+        text-decoration: none;
+      }
+    }
+  }
+}
+}
+// --------------TABLETTE---------------
+@media screen and (min-width: 600px) {
+  body {
+    #app {
+      main {
+            h1 {
+      font-family: nimbusBold;
+      font-size: 2.2rem;
+    }
+    h2 {
+      font-size: 1.4rem;
+      a {
+        font-size: 1.8rem;
+      }
+    }
+      }
+    }
+  }
 }
 </style>

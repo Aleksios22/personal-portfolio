571d9a1a73790092e030d8094934932d8417ec36
Alexis <e1577094@cmaisonneuve.qc.ca>
2021-03-07 18:32:10 -0500

    Text présentation

--- src/App.vue
+++ src/App.vue
@@ -20,65 +20,15 @@ export default {
 <style lang="scss">
 // --------------MOBILE FIRST---------------
 @font-face {
-  font-family: nimbusReg;
-  src: url("./assets/Fonts/nimbus-mono/nimbusmono-regular.otf");
-}
-@font-face {
-  font-family: nimbusBold;
-  src: url("./assets/Fonts/nimbus-mono/nimbusmono-bold.otf");
-}
-@font-face {
   font-family: barlowBold;
   src: url("./assets/Fonts/Barlow/Barlow-Bold.ttf");
 }
-body {
-  background: #ffffff;
-  #app {
-    width: 100vw;
-    height: 100vh;
-    font-family: "nimbusReg";
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 0 auto;
-    main {
-      text-align: center;
-      h1 {
-        font-family: "nimbusBold";
-        font-size: 1.6rem;
-      }
-      h2 {
-        font-size: 0.9rem;
-        a {
-          color: #85b36b;
-          font-size: 1.2rem;
-          text-decoration: none;
-        }
-      }
-    }
-  }
+@font-face {
+  font-family: barlowBoldItalic;
+  src: url("./assets/Fonts/Barlow/Barlow-BoldItalic.ttf");
 }
-// --------------TABLETTE---------------
-@media screen and (min-width: 640px) {
-  body {
-    #app {
-      main {
-        h1 {
-          font-size: 2.2rem;
-        }
-        h2 {
-          font-size: 1.4rem;
-          a {
-            font-size: 1.8rem;
-          }
-        }
-      }
-    }
-  }
-}
-// --------------DESKTOP---------------
-@media screen and (min-width: 1008px) {
-  body {
-  }
+@font-face {
+  font-family: IBMPlexMonoSemiBold;
+  src: url("./assets/Fonts/IBM-Plex-Mono/IBMPlexMono-SemiBold.ttf");
 }
 </style>
--- src/components/PanPresentation.vue
+++ src/components/PanPresentation.vue
@@ -1,5 +1,20 @@
 <template>
-  <article class="w-6/12 flex bg-white border-black border-l-4"></article>
+  <article class="w-6/12 flex flex-col bg-white border-black border-l-4">
+    <h1
+      style="font-family: barlowBoldItalic; line-height: 1.15"
+      class="text-6xl text-left tracking-wider mt-9 ml-9"
+    >
+      Je m'appelle <br />
+      Alexis Rochon,
+    </h1>
+    <h2
+      style="font-family: IBMPlexMonoSemiBold; line-height: 1.25"
+      class="text-4xl text-left mt-9 ml-9 tracking-widest"
+    >
+      Je suis un extraverti sensible, je brûle <br />
+      pour le développement front-end et j'ai soif d'apprendre.
+    </h2>
+  </article>
 </template>
 
 <script>

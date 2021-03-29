f9bf5c2a7d8cdc04a9e62cb61f8367e68732c773
Alexis <e1577094@cmaisonneuve.qc.ca>
2021-03-29 17:54:00 -0400

    lien fonctionnel

--- src/components/BtnPyramideAcc.vue
+++ src/components/BtnPyramideAcc.vue
@@ -1,12 +1,12 @@
 <template>
     <router-link :to="'/'" :style="{ width: largeur, letterSpacing: espacement }"
         class="btn text-6xl text-center flex items-center justify-center" style="
-        width: 100px;
-        height: 100px;
+        width: 150px;
+        height: 120px;
         position: absolute;
         align-self: flex-start;
         justify-self: start;
-        z-index: 20;
+        z-index: 100;
         margin-top: -2rem;
         margin-left: -1.8rem;">
     </router-link>
--- src/components/DescProjet.vue
+++ src/components/DescProjet.vue
@@ -3,6 +3,10 @@
     style="height: 100%; width: 100%; font-family: IBMPlexMonoSemiBold"
     class="flex border-black border-t-4 tracking-widest text-lg p-6 z-20 bg-white"
   >
+    <b><a target="_blank" :href="descProjet[indexProjet].lien">
+      lien
+    </a></b>
+
     <p
       >{{descProjet[indexProjet].description}}</p
     >
